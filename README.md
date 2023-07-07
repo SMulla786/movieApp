b9aee23c8db2e7094176d0294740aab8c57730dd
Suhel <Suhelmulla24@gmail.com>
2023-07-08 01:00:09 +0530

    mct done

--- README.md
+++ README.md
@@ -1,4 +1,33 @@
-# movieApp
-mct project using api
-link - https://smulla786.github.io/movieApp/
-resister yourself
+# Movie Information App
+
+## Technology Used
+
+- HTML: The app uses HTML for structuring the web page and defining its elements.
+- CSS: CSS is used for styling the app and applying visual enhancements to the elements.
+- JavaScript: The functionality of the app is implemented using JavaScript. It handles API requests, data manipulation, and event handling.
+
+## Terms Used
+
+- API: API stands for Application Programming Interface. In this project, the app interacts with The Movie Database (TMDB) API to fetch movie data such as trending movies and movie details.
+- Fetch: Fetch is a built-in JavaScript function used to make asynchronous HTTP requests to fetch data from a specified URL. It is used to fetch movie data from the TMDB API.
+- DOM Manipulation: DOM (Document Object Model) manipulation refers to the process of accessing and modifying HTML elements and their properties using JavaScript. It is used in this project to dynamically create and update movie cards and modal windows.
+- Modal Window: A modal window is a dialog box or popup that appears on top of the web page content, often used to display additional information or interactive features. In this project, modal windows are used to show detailed movie information when a movie card is clicked.
+- Local Storage: Local storage is a web browser feature that allows data to be stored locally on the user's device. In this project, existing user registrations are stored in local storage to enable user registration and sign-in functionality.
+- JSON: JSON (JavaScript Object Notation) is a lightweight data-interchange format. It is used in this project to parse and handle data received from the TMDB API.
+
+## Running the Project
+
+To run the project, follow these steps:
+
+1. Open a web browser.
+2. Visit the following URL: [https://smulla786.github.io/movieApp/](https://smulla786.github.io/movieApp/)
+3. The web page will load, displaying information about trending movies and movies currently playing.
+4. Click on a movie card to open a modal window with detailed information about the movie.
+5. Close the modal window by clicking on the close button or outside the modal area.
+6. Explore the app by navigating through different movies and viewing their details.
+
+Note: Make sure you have a stable internet connection as the app fetches data from the TMDB API in real-time.
+
+Feel free to navigate through the app and interact with its features. Enjoy exploring movie information!
+
+If you have any further questions or need assistance, feel free to ask.
--- script2.js
+++ script2.js
@@ -13,52 +13,38 @@ fetch(apiUrl)
       const card = document.createElement("div");
       card.className = "movie-card";
       card.innerHTML = `
-  <img src="${posterUrl}" alt="">
-  <p>${movie.title}</p>
-  <p>&#11088 ${movie.vote_average}</p>
-`;
-      let newMovie = null;
-
-      fetch(
-        `https://api.themoviedb.org/3/movie/${movie.id}?api_key=${apiKey}&append_to_response=casts,videos,images,release_dates`
-      )
-        .then((response) => response.json())
-        .then((NewData) => {
-          newMovie = NewData;
-
-          // access the newMovie variable here or perform any further operations with the data
-          console.log(newMovie);
-        })
-        .catch((error) => {
-          console.error("Error fetching movie details:", error);
-        });
-
-      // Outside of the fetch function, the value of newMovie will be null until the fetch is complete and the data is assigned to it
-      console.log(newMovie);
+        <img src="${posterUrl}" alt="">
+        <p>${movie.title}</p>
+        <p>&#11088 ${movie.vote_average}</p>
+      `;
 
       const modal = document.createElement("div");
       modal.className = "modal";
       modal.innerHTML = `
-  <div class="modal-content">
-    <span class="close">&times;</span>
-    <div class="modal-page">
-    <div class="modal-image">
-        <img src="https://image.tmdb.org/t/p/w185${movie.poster_path}" alt="">
-        </div>
-        <div class="modal-text">
-    
-    <h2> ${movie.title}</h2>
-    <p><pre> ${movie.release_date.slice(0, 4)} | &#11088 ${
+        <div class="modal-content">
+          <span class="close">&times;</span>
+          <div class="modal-page">
+            <div class="modal-image">
+              <img src="https://image.tmdb.org/t/p/w185${
+                movie.poster_path
+              }" alt="">
+            </div>
+            <div class="modal-text">
+              <h2>${movie.title}</h2>
+              <p>${movie.release_date.slice(0, 4)} | &#11088 ${
         movie.vote_average
-      }</pre></p>
-    <hr>
-    <p>${movie.overview}</p>
-</div>
-</div>
-  </div>
-`;
+      }</p>
+              <p class="cast"></p>
+              <hr>
+              <p>${movie.overview}</p>
+            </div>
+          </div>
+        </div>
+      `;
+
       card.addEventListener("click", () => {
         modal.style.display = "block";
+        fetchCast(movie.id); // Fetch cast information when the card is clicked
       });
       modal.querySelector(".close").addEventListener("click", () => {
         modal.style.display = "none";
@@ -68,6 +54,50 @@ fetch(apiUrl)
     });
   })
   .catch((error) => console.log(error));
+
+// Function to fetch and display cast information for a given movie ID
+function fetchCast(movieId) {
+  const castUrl = `https://api.themoviedb.org/3/movie/${movieId}/credits?api_key=${apiKey}`;
+
+  fetch(castUrl)
+    .then((response) => response.json())
+    .then((data) => {
+      const cast = data.cast.slice(0, 5);
+      const castContainer = document.querySelector(".cast");
+      castContainer.innerHTML = "<strong>Cast:</strong> ";
+
+      cast.forEach((actor, index) => {
+        castContainer.innerHTML += actor.name;
+
+        if (index < cast.length - 1) {
+          castContainer.innerHTML += ", ";
+        }
+      });
+    })
+    .catch((error) => console.error("Error fetching cast information:", error));
+}
+
+// Function to fetch and display cast information for a given movie ID
+function fetchCast(movieId) {
+  const castUrl = `https://api.themoviedb.org/3/movie/${movieId}/credits?api_key=${apiKey}`;
+
+  fetch(castUrl)
+    .then((response) => response.json())
+    .then((data) => {
+      const cast = data.cast.slice(0, 5);
+      const castContainer = document.querySelector(".cast");
+      castContainer.innerHTML = "<strong>Cast:</strong> ";
+
+      cast.forEach((actor, index) => {
+        castContainer.innerHTML += actor.name;
+
+        if (index < cast.length - 1) {
+          castContainer.innerHTML += ", ";
+        }
+      });
+    })
+    .catch((error) => console.error("Error fetching cast information:", error));
+}
 
 // const apiUrl2 = `https://api.themoviedb.org/3/movie/343611?api_key=${apiKey}`;
 // var movieData;
@@ -81,7 +111,7 @@ fetch(apiUrl)
 //   .catch((error) => console.error(error)); // handle any errors
 // console.log(movieData);
 
-const apiUrl3 = `https://api.themoviedb.org/3/movie/now_playing?api_key=${apiKey}`;
+const apiUrl3 = `https://api.themoviedb.org/3/movie/now_playing?api_key=${apiKey}&append_to_response=casts,videos,images,release_dates`;
 
 fetch(apiUrl3)
   .then((response) => response.json())
@@ -90,6 +120,7 @@ fetch(apiUrl3)
     const container = document.getElementsByClassName("movie-cards")[1];
     container.innerHTML = `<h2 style="width:100%;">Now playing Movies</h2>`;
     movies.forEach((movie) => {
+      console.log(movie);
       const posterUrl = `https://image.tmdb.org/t/p/w185${movie.poster_path}`;
       const card = document.createElement("div");
       card.className = "movie-card";
@@ -102,7 +133,7 @@ fetch(apiUrl3)
       const modal = document.createElement("div");
       modal.className = "modal";
       modal.innerHTML = `
-  <div class="modal-content">
+  <div  class="modal-content">
     <span class="close">&times;</span>
     <div class="modal-page">
     <div class="modal-image">
@@ -122,6 +153,7 @@ fetch(apiUrl3)
 `;
       card.addEventListener("click", () => {
         modal.style.display = "block";
+        fetchCast(movie.id);
       });
       modal.querySelector(".close").addEventListener("click", () => {
         modal.style.display = "none";
@@ -131,3 +163,18 @@ fetch(apiUrl3)
       container.appendChild(card);
     });
   });
+function Recomendation(id) {
+  fetch(
+    `https://api.themoviedb.org/3/movie/${id}?api_key=${apiKey}&append_to_response=casts,videos,images,release_dates`
+  )
+    .then((response) => response.json())
+    .then((NewData) => {
+      newMovie = NewData;
+
+      // access the newMovie variable here or perform any further operations with the data
+      console.log(newMovie);
+    })
+    .catch((error) => {
+      console.error("Error fetching movie details:", error);
+    });
+}
